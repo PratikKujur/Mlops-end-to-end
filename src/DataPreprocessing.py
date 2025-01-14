baf81b5e5cea9a2c59a048b10578de4db2e56431
PratikKujur <pratikkujur121@gmail.com>
2025-01-14 15:43:12 +0530

    experiment initialized and Data Pre-Processing added

--- src/DataPreprocessing.py
+++ src/DataPreprocessing.py
@@ -8,12 +8,15 @@ params=yaml.safe_load(open("/Users/pratik.kujur/Desktop/Projects/Mlops-end-to-en
 
 def DataPreprocessing(input_path,output_path):
     df=pd.read_csv(input_path)
+    print("Raw Data \n",df.head())
+    # add preprocessing step here
+
+    unecessary_col=['Unnamed: 0']
+    df.drop(labels=unecessary_col,axis='columns',inplace=True)
+    
     os.makedirs(os.path.dirname(output_path),exist_ok=True)
-
-    df.to_csv(output_path,header=False,index=False)
-    print(f"Processed file is saved in this Location->{output_path}")
-
-if __name__=="__main__":
-    DataPreprocessing(params['input'],params['output'])
+    df.to_csv(output_path,index=False,header=False)
+    print("Preprocessed Data \n",df.head())
+    print(f"Preprocessed data is saved in this location -> {output_path}")
 
 
---
+++ src/ModelTrainer.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+import sys
+import yaml
+
